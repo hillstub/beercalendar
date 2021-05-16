c3a679d6cd3b1429ee397ed724c9c5eecbc07afc
H.M. van Rossum <hmvanrossum@MacBook-Air-van-HM.local>
2021-05-16 20:39:36 +0200

    added additional code

--- get_gsheets_posts.py
+++ get_gsheets_posts.py
@@ -2,4 +2,20 @@
 import os
 
 print("===== get posts from gsheet ===")
-print(os.environ("GSPREAD_TYPE"))
+print(os.environ["GSPREAD_TYPE"])
+
+
+filename = "2021-05-15-hallo.markdown"
+f = open(f"_posts/{filename}", "w")
+f.write('''---
+layout: post
+title:  "Welcome to Jekyll!"
+---
+
+# Welcome Mens
+
+**Hello world**, this is my first Jekyll blog post.
+
+I hope you like it!
+''')
+f.close()
